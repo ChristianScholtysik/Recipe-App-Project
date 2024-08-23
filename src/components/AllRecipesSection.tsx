76ca217963bd69c6d8d73901e9eb4f45812c80a5
Christian Scholtysik <christianscholtysik@gmail.com>
2024-08-23 08:34:31 +0200

    cleaning code

--- src/components/AllRecipesSection.tsx
+++ src/components/AllRecipesSection.tsx
@@ -1,22 +1,3 @@
-// import { useNavigate } from "react-router-dom";
-
-// const AllRecipesSection = () => {
-//   const navigate = useNavigate();
-//   return (
-//     <section className="flex flex-col items-center justify-center m-16">
-//       <h2 className="font-inter font-bold text-xl text-tBase mb-16">
-//         Alle Rezepte
-//       </h2>
-//       <button
-//         className="bg-primary text-tBase py-3 px-8 rounded-full hover:bg-hover font-inter font-bold "
-//         onClick={() => navigate("/all-recipes")}>
-//         Zu den Rezepten
-//       </button>
-//     </section>
-//   );
-// };
-
-// export default AllRecipesSection;
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import supabaseClient from "../lib/supabaseClient";
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,10 +5,8 @@ const Header = () => {
     <section className="relative h-72">
       <img src={Hero} alt="Hero" className="h-72 w-full object-cover" />
 
-      {/* Dark overlay for better text contrast */}
       <div className="absolute inset-0 bg-black opacity-50 z-10" />
 
-      {/* Centered text content */}
       <div className="absolute inset-0 flex items-center justify-center z-20">
         <p className="text-white font-inter font-bold text-center text-xl md:text-2xl lg:text-3xl w-3/4 md:w-2/3 lg:w-1/2 px-4">
           Lassen Sie sich inspirieren, kochen Sie mit Leidenschaft und erleben
